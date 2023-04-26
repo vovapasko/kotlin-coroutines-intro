b049e1d584a3e71d2e3b6399ec224343915f8c49
vovapasko <vovapasko1699@gmail.com>
2023-04-26 18:46:14 +0200

    feat: implemented aggregate function

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,16 +3,30 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
-
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
  Merge duplications: each user should be present only once in the resulting list
  with the total value of contributions for all the repositories.
- Users should be sorted in a descending order by their contributions.
+ Users should be sorted in descending order by their contributions.
 
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> {
+    val userMap = HashMap<String, Int>()
+    val newList = ArrayList<User>()
+    this.forEach { user ->
+        if (userMap.contains(user.login)) {
+            var oldValue = userMap[user.login]!!
+            oldValue += user.contributions
+            userMap[user.login] = oldValue
+        }
+        else{
+            userMap[user.login] = user.contributions
+        }
+    }
+    userMap.forEach { element ->
+        newList.add(User(element.key, element.value))
+    }
+    return newList
+}
