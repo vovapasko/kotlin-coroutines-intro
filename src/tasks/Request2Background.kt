aa2b24eb043cfc2bd5d71a1ead6072121ed871b8
vovapasko <vovapasko1699@gmail.com>
2023-04-27 16:21:11 +0200

    feat: completed task 2

--- src/tasks/Request2Background.kt
+++ src/tasks/Request2Background.kt
@@ -7,6 +7,7 @@ import kotlin.concurrent.thread
 
 fun loadContributorsBackground(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     thread {
-        loadContributorsBlocking(service, req)
+        val loadedUsers = loadContributorsBlocking(service, req)
+        updateResults(loadedUsers)
     }
 }
