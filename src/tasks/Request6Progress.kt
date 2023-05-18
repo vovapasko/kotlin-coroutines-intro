2aee25e502d2d73419fe0ca1f076380b2bd573aa
vovapasko <vovapasko1699@gmail.com>
2023-05-18 14:59:35 +0200

    feat: completed task 7

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,21 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+    val currentUsers = mutableListOf<User>()
+    val users = repos.flatMap { repo ->
+        service
+            .getRepoContributors(req.org, repo.name)
+            .also {
+                logUsers(repo, it)
+            }
+            .bodyList().also {
+                currentUsers += it
+                updateResults(currentUsers.aggregate(), false)
+            }
+    }
+    updateResults(users, true)
 }
